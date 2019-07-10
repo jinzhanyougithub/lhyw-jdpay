0beed1c9456dabb9f0670eb34a971bb28921342d
jinzhanyou <jinzhanyou@enet360.com>
2019-07-10 18:42:54 +0800

    update

--- src/JdPay.php
+++ src/JdPay.php
@@ -4,8 +4,22 @@ namespace Lhyw\JdPay;
 
 class JdPay
 {
-    public static function execute($data)
+    public static function execute($config)
     {
+        $data = [];
+
+        $data['v_mid'] = config('jd.v_mid'); //商户ID
+        $data['v_url'] = config('jd.v_url'); //支付完成 返回地址
+        $data['key'] = config('jd.key');
+        $data['remark2'] = config('jd.remark2'); // 支付完成 异步通知
+        $data['v_oid'] = $config['order_sn']; //订单号
+        $data['v_amount'] = 1; //订单金额
+        $data['v_moneytype'] = 'CNY'; //金额类型
+        $text = $data['v_amount'] . $data['v_moneytype'] . $data['v_oid'] . $data['v_mid'] . $data['v_url'] . $data['key'];
+        $data['v_md5info'] = strtoupper(md5($text)); //MD5加密
+        $data['remark1'] = '保证金支付'; //备注
+        $data['pmode_id'] = 3080; //银行ID
+
         return $data;
     }
 }
--- src/config/jd.php
+++ src/config/jd.php
@@ -1,3 +1,9 @@
 <?php
-return [];
+return [
+    'v_mid' => '1001',
+    'v_url' => 'http://localhost/chinabak/Receive.php',
+    'key' => 'test',
+    'remark2' => '[url:=http://domain/chinabank/AutoReceive.php]',
+    'gateway' => 'https://tmapi.jdpay.com/PayGate'
+];
 ?>
